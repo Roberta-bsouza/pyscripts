efa1e6bc4b732f14d77ae89a4cedcb301190f143
Roberta Souza <62950737+Roberta-bsouza@users.noreply.github.com>
2020-08-12 20:26:08 -0300

    Novos exercícios

--- exe011.py
+++ exe011.py
@@ -3,3 +3,5 @@ suc = n1 + 1
 ant = n1 - 1
 print('O sucessor de', n1 ,'é igual a {}'.format(suc))
 print('O antecessor de', n1 ,'é igual a {}'.format(ant))
+
+#print('O sucessor do valor {}, e {} e o antecessor é {}.'.format(n, n+1, n-1))
