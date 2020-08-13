7edd346681fb688b47e89aad8940c4fc2b8aa903
Roberta Souza <62950737+Roberta-bsouza@users.noreply.github.com>
2020-08-12 21:37:13 -0300

    Estudando Módulos

---
+++ exe024.py
@@ -0,0 +1,10 @@
+#importando módulos(pacotes)
+#é preciso fazer adições para que os programas utilizem algumas funcionalidades
+#essas funcionalidades podem ser chamadas de bibliotecas
+#from math import sqrt,floor
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+#print('A raiz de {} é igual a {}'.format(num, floor(raiz)))
+import random #números ale
+num = random.randint(1,10)
+print
