1269a33234a4c04391c1e373b18fdefdb500e57c
Roberta Souza <62950737+Roberta-bsouza@users.noreply.github.com>
2020-08-13 21:49:14 -0300

    Commit em Estudos-Python

--- exe031.py
+++ exe031.py
@@ -1,4 +1,9 @@
 #trabalhando com manipulação de strings
 #fatiamento de string
 frase = 'Curso em vídeo python'
-print(frase[13:])
+print(frase[::2])
+print(frase.count('0'))
+print(frase.upper().count('0'))
+print(len(frase.strip()))
+print(frase.replace('Python', 'Android'))
+print(frase.strip())
